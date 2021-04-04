d7b0e8ecc2946589702c3363832a5b52a86f88ef
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-04 19:34:52 +0900

    FIX; already Signup Exception

--- Authorization/src/shared/exception/exception.index.ts
+++ Authorization/src/shared/exception/exception.index.ts
@@ -24,7 +24,7 @@ export const unauthorizedSecretKey = new UnauthorizedException(
   "Unauthorized Secret Key",
 );
 export const forbiddenCodeException = new ForbiddenException("Forbidden Code");
-export const aleadySignupException = new ForbiddenException("Aleady Signup");
+export const alreadySignupException = new ForbiddenException("Already Signup");
 export const notFoundEmailException = new NotFoundException("Not Found Email");
 export const notAllowedIDException = new MethodNotAllowedException(
   "Not Allowed ID",
