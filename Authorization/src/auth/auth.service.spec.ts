4e029bbb657c727e45dedc4039f3687bc0d5b369
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-04 19:36:17 +0900

    UPDATE; auth.service.ts, auth.service.spec.ts
    Add email Authentication Method

--- Authorization/src/auth/auth.service.spec.ts
+++ Authorization/src/auth/auth.service.spec.ts
@@ -1,14 +1,27 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { getRepositoryToken } from "@nestjs/typeorm";
+import { notFoundEmailException } from "src/shared/exception/exception.index";
 import { User } from "../shared/user/entity/user.entity";
 import { AuthService } from "./auth.service";
 
 class MockRepository {
-  public async findOne(id: string): Promise<User> {
-    if (id === "tester") {
-      throw new User();
+  public async checkExist(identity: string): Promise<boolean> {
+    if (identity === "tester") {
+      return true;
     } else {
-      return undefined;
+      return false;
+    }
+  }
+
+  public async findOne({ where: { email } }): Promise<User> {
+    if (email === "201216jjw@dsm.hs.kr") {
+      return new User();
+    } else if (email === "alreadysignupeamil@dsm.hs.kr") {
+      const user = new User();
+      user.password = "exist";
+      return user;
+    } else {
+      undefined;
     }
   }
 }
@@ -36,14 +49,40 @@ describe("AuthService", () => {
 
   describe("checkAllowedId", () => {
     it("should not allowed id", () => {
-      service
-        .checkAllowedId("tester")
-        .catch((err) => expect(err.message).toEqual("Not Allowed ID"));
+      service.checkAllowedId("tester").catch((err) => {
+        expect(err.message).toEqual("Not Allowed ID");
+        expect(err.getStatus()).toEqual(405);
+      });
     });
 
     it("should allowed id", () => {
       service
         .checkAllowedId("not tester")
+        .then(() => expect(1).toEqual(1))
+        .catch(() => expect(1).toEqual(2));
+    });
+  });
+
+  describe("emailAuthentication", () => {
+    it("should throw not fount email error", () => {
+      service.emailAuthentication("1234").catch((err) => {
+        expect(err.getStatus()).toEqual(404);
+        expect(err.message).toEqual("Not Found Email");
+      });
+    });
+
+    it("should throw already signup error", () => {
+      service
+        .emailAuthentication("alreadysignupeamil@dsm.hs.kr")
+        .catch((err) => {
+          expect(err.getStatus()).toEqual(403);
+          expect(err.message).toEqual("Already Signup");
+        });
+    });
+
+    it("shoul success test", () => {
+      service
+        .emailAuthentication("201216jjw@dsm.hs.kr")
         .then(() => expect(1).toEqual(1))
         .catch(() => expect(1).toEqual(2));
     });
--- Authorization/src/auth/auth.service.ts
+++ Authorization/src/auth/auth.service.ts
@@ -1,14 +1,53 @@
-import { Injectable } from "@nestjs/common";
-import { notAllowedIDException } from "../shared/exception/exception.index";
+import { Injectable, Logger } from "@nestjs/common";
+import { asyncFuncRedisSet } from "../redis.client";
+import { User } from "../shared/user/entity/user.entity";
+import {
+  alreadySignupException,
+  notAllowedIDException,
+  notFoundEmailException,
+} from "../shared/exception/exception.index";
 import { UserRepository } from "../shared/user/entity/user.repository";
+import { sendMail } from "../shared/mail/mail.transport";
 
 @Injectable()
 export class AuthService {
   constructor(private userRepository: UserRepository) {}
 
   public async checkAllowedId(identity: string) {
-    if (await this.userRepository.findOne({ where: { identity } })) {
+    if (await this.userRepository.checkExist(identity)) {
       throw notAllowedIDException;
+    }
+  }
+
+  public async emailAuthentication(email: string) {
+    const existUser: User = await this.userRepository.findOne({
+      where: { email },
+    });
+    if (!existUser) {
+      throw notFoundEmailException;
+    }
+    if (existUser.password) {
+      throw alreadySignupException;
+    }
+    const authNum: string = Math.floor(Math.random() * 1000000).toString();
+    this.setAuthNumberForEamil(email, authNum);
+    this.sendEmailWithAuthNumber(email, authNum);
+  }
+
+  private async setAuthNumberForEamil(email: string, authNum: string) {
+    try {
+      await asyncFuncRedisSet(email, authNum);
+    } catch (err) {
+      Logger.error(err);
+    }
+  }
+
+  private async sendEmailWithAuthNumber(email: string, authNum: string) {
+    try {
+      const result = await sendMail(email, authNum);
+      Logger.log(result);
+    } catch (err) {
+      Logger.error(err);
     }
   }
 }
