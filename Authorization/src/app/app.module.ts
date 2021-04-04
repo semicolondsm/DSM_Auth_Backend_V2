c2d7b5bd257e4f1e7b6520e18fa88a2bb2ade22e
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-04 19:36:32 +0900

    REFACTOR; app.module.ts

--- Authorization/src/app/app.module.ts
+++ Authorization/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from "@nestjs/common";
-import { JwtService } from "@nestjs/jwt";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthModule } from "src/auth/auth.module";
 import { connectionOptions } from "src/ormconfig";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-  imports: [TypeOrmModule.forRoot(connectionOptions[process.env.NODE_ENV])],
+  imports: [
+    TypeOrmModule.forRoot(connectionOptions[process.env.NODE_ENV]),
+    AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, JwtService],
+  providers: [AppService],
 })
 export class AppModule {}
