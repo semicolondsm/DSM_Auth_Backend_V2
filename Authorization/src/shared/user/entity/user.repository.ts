026f1760058b42aaedda0980d10ab139781b32a8
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-04 19:14:58 +0900

    UPDATE; user.repository.ts
    Add CheckExist Method

--- Authorization/src/shared/user/entity/user.repository.ts
+++ Authorization/src/shared/user/entity/user.repository.ts
@@ -2,4 +2,15 @@ import { EntityRepository, Repository } from "typeorm";
 import { User } from "./user.entity";
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  public async checkExist(identity: string): Promise<boolean> {
+    const user = this.createQueryBuilder("user")
+      .select("user.identity")
+      .where("user.identity = :identity", { identity })
+      .getOne();
+    if (user) {
+      return true;
+    }
+    return false;
+  }
+}
