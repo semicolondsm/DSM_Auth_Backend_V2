a8c5ce0fa9aa242c064a9e7c789aa360504fa2eb
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-04 14:00:50 +0900

    CREATE; exception.index.ts

---
+++ Authorization/src/shared/exception/exception.index.ts
@@ -0,0 +1,30 @@
+import {
+  BadRequestException,
+  ForbiddenException,
+  MethodNotAllowedException,
+  NotFoundException,
+  UnauthorizedException,
+} from "@nestjs/common";
+
+export const badRequestException = new BadRequestException();
+export const unauthorizedTokenException = new UnauthorizedException(
+  "Unauthorized Token",
+);
+export const expiredTokenException = new UnauthorizedException("Expired Token");
+export const unauthorizedCodeException = new UnauthorizedException(
+  "Unauthorized Code",
+);
+export const unauthorizedIDException = new UnauthorizedException(
+  "Unauthorized ID",
+);
+export const unauthorizedPasswordException = new UnauthorizedException(
+  "Unauthorized password",
+);
+export const unauthorizedSecretKey = new UnauthorizedException(
+  "Unauthorized Secret Key",
+);
+export const forbiddenCodeException = new ForbiddenException("Forbidden Code");
+export const notFoundEmailException = new NotFoundException("Not Found Email");
+export const notAllowedIDException = new MethodNotAllowedException(
+  "Not Allowed ID",
+);
