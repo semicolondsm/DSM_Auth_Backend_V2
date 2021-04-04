b8028af0e8f47e57ad49cfc2fef75d38331fc34f
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-04 18:40:05 +0900

    CREATE; mail transport module

---
+++ Authorization/src/shared/mail/mail.transport.ts
@@ -0,0 +1,32 @@
+import nodemailer from "nodemailer";
+import path from "path";
+import fs from "fs";
+
+const HTMLTemplate: string = fs.readFileSync(path.join(__dirname, "./mail.template.html"), { encoding: "utf-8" });
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail', 
+  host: "smtp.gmail.com",
+  port: 465,  
+  secure: true,
+  auth: { 
+    user: process.env.GMAIL_USER, 
+    pass: process.env.GMAIL_PASSWORD 
+  }
+});
+
+const sendMail = (userMail: string, authNum: string): Promise<object> => {
+  return new Promise((resolve, reject) => {
+    transporter.sendMail({
+      from: process.env.GMAIL_USER,
+      to: userMail,
+      subject: `DSM_Auth 이메일 승인코드 to ${userMail}`,
+      html: HTMLTemplate.replace("EmailAuthenticationCode", authNum),
+    }, (err, res) => {
+      if(err) reject(err);
+      else resolve(res);
+    });
+  });
+}
+
+export { sendMail }
