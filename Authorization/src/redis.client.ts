86b090b62dab67596f2cd9e2816880a2b4beedef
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-04 17:24:55 +0900

    CREATE; redis.client.ts
    Create promisify redis functions

---
+++ Authorization/src/redis.client.ts
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import redis from "redis";
+import { promisify } from "util";
+
+const redisClient: redis.RedisClient = redis.createClient({
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  password: process.env.REDIS_PASSWORD,
+});
+
+const asyncFuncRedisGet = promisify(redisClient.get).bind(redisClient);
+const asyncFuncRedisSet = promisify(redisClient.set).bind(redisClient);
+
+export default redisClient;
+export { asyncFuncRedisGet, asyncFuncRedisSet };
