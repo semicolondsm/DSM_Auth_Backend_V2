fd2dbbce171a73553abf5791f544e7a8cc90e6a3
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-08 21:56:04 +0900

    UPDATE; user.service.ts Add findByIdentity Mehtod

--- Authorization/src/shared/user/user.service.ts
+++ Authorization/src/shared/user/user.service.ts
@@ -4,9 +4,13 @@ import { UserRepository } from "./entity/user.repository";
 
 @Injectable()
 export class UserService {
-  constructor(private userRepository: UserRepository) {}
+  constructor(private readonly userRepository: UserRepository) {}
 
   public findById(user_id: number): Promise<User> {
     return this.userRepository.findOne(user_id);
+  }
+
+  public findByIdentity(identity: string): Promise<User> {
+    return this.userRepository.findOne({ where: { identity } });
   }
 }
