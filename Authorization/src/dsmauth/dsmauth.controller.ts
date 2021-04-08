585431a12f09f8a374c37b7fdb7b317096cf615d
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-08 22:21:13 +0900

    CREATE; dsmauth modules

---
+++ Authorization/src/dsmauth/dsmauth.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('dsmauth')
+export class DsmauthController {}
---
+++ Authorization/src/dsmauth/dsmauth.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { DsmauthService } from './dsmauth.service';
+import { DsmauthController } from './dsmauth.controller';
+
+@Module({
+  providers: [DsmauthService],
+  controllers: [DsmauthController]
+})
+export class DsmauthModule {}
