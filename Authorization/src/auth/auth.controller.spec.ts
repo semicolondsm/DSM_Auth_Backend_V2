820b80f664fbcdd3cc5e542efb4802fa84cb77b9
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-08 18:32:55 +0900

    UPDATE; auth.controller.spec.ts, auth.module.ts
    adapt nestjs config module, add auth controller service

--- Authorization/src/auth/auth.controller.spec.ts
+++ Authorization/src/auth/auth.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { getRepositoryToken } from "@nestjs/typeorm";
+import { notAllowedIDException } from "../shared/exception/exception.index";
 import { User } from "../shared/user/entity/user.entity";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
@@ -7,9 +8,19 @@ import { AuthService } from "./auth.service";
 class MockRepository {
   public async findOne(id: string): Promise<User> {
     if (id === "tester") {
-      throw new User();
+      return new User();
     } else {
       return undefined;
+    }
+  }
+}
+
+class MockAuthService {
+  public checkAllowedId(id: string): Promise<void> {
+    if (id === "allowId") {
+      return;
+    } else {
+      throw notAllowedIDException;
     }
   }
 }
@@ -20,7 +31,10 @@ describe("AuthController", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
-        AuthService,
+        {
+          provide: AuthService,
+          useClass: MockAuthService,
+        },
         {
           provide: getRepositoryToken(User),
           useClass: MockRepository,
@@ -34,5 +48,21 @@ describe("AuthController", () => {
 
   it("should be defined", () => {
     expect(controller).toBeDefined();
+  });
+
+  describe("checkAllowedId", () => {
+    it("should be allowed", () => {
+      controller
+        .checkAllowedId({ id: "allowId" })
+        .then((res) => expect(res.message).toEqual("Allowed ID"))
+        .catch(() => expect(1).toEqual(2));
+    });
+
+    it("should be not allowed", () => {
+      controller
+        .checkAllowedId({ id: "not allowed id" })
+        .then(() => expect(1).toEqual(2))
+        .catch((err) => expect(err.getStatus()).toEqual(405));
+    });
   });
 });
--- Authorization/src/auth/auth.module.ts
+++ Authorization/src/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UserRepository } from "../shared/user/entity/user.repository";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [TypeOrmModule.forFeature([UserRepository]), ConfigModule],
   providers: [AuthService],
   controllers: [AuthController],
 })
