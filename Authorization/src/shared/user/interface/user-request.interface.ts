35d4fbc04c168cce80444d8ce71045855fb2efc7
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-10 23:31:18 +0900

    DELETE; user-request.interface.ts reset commit

--- Authorization/src/shared/user/interface/user-request.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Request } from "express";
-import { IJwtPayload } from "../../../shared/jwt/interface/jwt-payload.interface";
-
-export interface IUserRequest extends Request {
-  user: IJwtPayload;
-}
