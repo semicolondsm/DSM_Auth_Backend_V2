c271b8299b12a04a8bc5070215e7047467176072
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-11 03:34:57 +0900

    UPDATE; jwt.strategy.ts add refresh strategy

--- Authorization/src/shared/jwt/passport/jwt.strategy.ts
+++ Authorization/src/shared/jwt/passport/jwt.strategy.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport";
-import { ExtractJwt } from "passport-jwt";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { unauthorizedTokenException } from "src/shared/exception/exception.index";
 import { IJwtPayload } from "../interface/jwt-payload.interface";
 import { JWT_SECRET_KEY } from "../jwt.constant";
 
@@ -16,6 +16,27 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   public async validate(payload: IJwtPayload) {
+    if(payload.type !== "access") {
+      throw unauthorizedTokenException;
+    }
+    return payload;
+  }
+}
+
+@Injectable()
+export class JwtRefreshStrategy extends PassportStrategy(Strategy, "jwt-refresh") {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: JWT_SECRET_KEY,
+    });
+  }
+
+  public async validate(payload: IJwtPayload) {
+    if(payload.type !== "refresh") {
+      throw unauthorizedTokenException;
+    }
     return payload;
   }
 }
