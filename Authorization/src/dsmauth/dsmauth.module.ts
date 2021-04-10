6d48a22c8f7354cce3a0f63f1c344ea1bf59deb8
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-10 23:32:01 +0900

    UPDATE; dsmauth.module.ts add jwt module

--- Authorization/src/dsmauth/dsmauth.module.ts
+++ Authorization/src/dsmauth/dsmauth.module.ts
@@ -5,9 +5,18 @@ import { ConfigModule } from "@nestjs/config";
 import { PassportModule } from "@nestjs/passport";
 import { LocalStrategy } from "../dsmauth/dsmauth.local.strategy";
 import { UserModule } from "../shared/user/user.module";
+import { JwtModule } from "@nestjs/jwt";
+import { JWT_SECRET_KEY } from "../shared/jwt/jwt.constant";
 
 @Module({
-  imports: [ConfigModule, PassportModule, UserModule],
+  imports: [
+    ConfigModule,
+    PassportModule,
+    UserModule,
+    JwtModule.register({
+      secret: JWT_SECRET_KEY,
+    }),
+  ],
   providers: [DsmauthService, LocalStrategy],
   controllers: [DsmauthController],
 })
