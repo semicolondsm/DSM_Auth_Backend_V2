23eec20533ea25b81933326600a64c17a57ac0dd
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-10 22:49:14 +0900

    CREATE; jwt.constant.ts define jwt constants

---
+++ Authorization/src/shared/jwt/jwt.constant.ts
@@ -0,0 +1,8 @@
+import "dotenv/config";
+
+export const ACCESS_TOKEN_EXPIRED_TIME = process.env.ACCESS_TOKEN_EXPIRED_TIME;
+export const REFRESH_TOKEN_EXPIRED_TIME =
+  process.env.REFRESH_TOKEN_EXPIRED_TIME;
+export const JWT_SECRET_KEY = process.env.JWT_SECRET;
+export const ACCESS_TOKEN_HEADER = "access-token";
+export const REFRESH_TOKEN_HEADER = "refresh-token";
