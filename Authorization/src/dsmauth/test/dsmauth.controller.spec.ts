cb2c265a26cec6ef8b1951ad27eceb9e2360356d
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-12 08:41:02 +0900

    UPDATE; dsmauth.service.spec.ts, dsmauth.controller.ts
    init testing file

--- Authorization/src/dsmauth/test/dsmauth.controller.spec.ts
+++ Authorization/src/dsmauth/test/dsmauth.controller.spec.ts
@@ -1,11 +1,36 @@
+import { JwtModule } from "@nestjs/jwt";
 import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { User } from "../../shared/user/entity/user.entity";
+import { UserService } from "../../shared/user/user.service";
 import { DsmauthController } from "../dsmauth.controller";
+import { DsmauthService } from "../dsmauth.service";
+
+class MockUserRepository {
+  public findOne({ where: { identity } }) {}
+}
+
+class MockUserService {
+  public findByIdentity(identity: number) {}
+}
 
 describe("DsmauthController", () => {
   let controller: DsmauthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [JwtModule.register({})],
+      providers: [
+        DsmauthService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: MockUserRepository,
+        },
+        {
+          provide: UserService,
+          useClass: MockUserService,
+        },
+      ],
       controllers: [DsmauthController],
     }).compile();
 
--- Authorization/src/dsmauth/test/dsmauth.service.spec.ts
+++ Authorization/src/dsmauth/test/dsmauth.service.spec.ts
@@ -1,12 +1,37 @@
+import { JwtModule } from "@nestjs/jwt";
 import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { User } from "../../shared/user/entity/user.entity";
+import { DsmauthController } from "../dsmauth.controller";
 import { DsmauthService } from "../dsmauth.service";
+import { UserService } from "../../shared/user/user.service";
+
+class MockUserRepository {
+  public findOne({ where: { identity } }) {}
+}
+
+class MockUserService {
+  public findByIdentity(identity: number) {}
+}
 
 describe("DsmauthService", () => {
   let service: DsmauthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DsmauthService],
+      imports: [JwtModule.register({})],
+      providers: [
+        DsmauthService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: MockUserRepository,
+        },
+        {
+          provide: UserService,
+          useClass: MockUserService,
+        },
+      ],
+      controllers: [DsmauthController],
     }).compile();
 
     service = module.get<DsmauthService>(DsmauthService);
