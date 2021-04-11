36fb127f63241baaf03a0c5694016b916cac225c
sonchaegeon <thscorjs1002@naver.com>
2021-04-11 19:15:35 +0900

    UPDATE; consumer module import repositories, jwt strategy

--- Authorization/src/consumer/consumer.module.ts
+++ Authorization/src/consumer/consumer.module.ts
@@ -1,8 +1,16 @@
 import { Module } from "@nestjs/common";
 import { ConsumerService } from "./consumer.service";
 import { ConsumerController } from "./consumer.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConsumerRepository } from "./entity/consumer.repository";
+import { UserRepository } from "src/shared/user/entity/user.repository";
+import { JwtStrategy } from "src/shared/jwt/passport/jwt.strategy";
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([ConsumerRepository, UserRepository]),
+    JwtStrategy,
+  ],
   providers: [ConsumerService],
   controllers: [ConsumerController],
 })
