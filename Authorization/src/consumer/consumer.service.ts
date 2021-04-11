29df83efbd2c4e49817fe750717097ced876816e
sonchaegeon <thscorjs1002@naver.com>
2021-04-11 19:15:56 +0900

    UPDATE; consumer registration service

--- Authorization/src/consumer/consumer.service.ts
+++ Authorization/src/consumer/consumer.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable, Scope } from "@nestjs/common";
+import { REQUEST } from "@nestjs/core";
+import { InjectRepository } from "@nestjs/typeorm";
+import { notFoundUserException } from "src/shared/exception/exception.index";
+import { IJwtPayload } from "src/shared/jwt/interface/jwt-payload.interface";
+import { User } from "src/shared/user/entity/user.entity";
+import { UserRepository } from "src/shared/user/entity/user.repository";
+import { v4 } from "uuid";
+import {
+  RegistrationDto,
+  RegistrationResponseData,
+} from "./dto/registration.dto";
+import { Consumer } from "./entity/consumer.entity";
+import { ConsumerRepository } from "./entity/consumer.repository";
 
-@Injectable()
-export class ConsumerService {}
+@Injectable({ scope: Scope.REQUEST })
+export class ConsumerService {
+  constructor(
+    @InjectRepository(Consumer)
+    private readonly consumerRepository: ConsumerRepository,
+    @InjectRepository(User) private readonly userRepository: UserRepository,
+    @Inject(REQUEST) private request,
+  ) {}
+
+  public async registration(
+    dto: RegistrationDto,
+  ): Promise<RegistrationResponseData> {
+    const user: User = await this.getUser();
+    if (!user) {
+      throw notFoundUserException;
+    }
+    return this.consumerRepository.registration(dto, user);
+  }
+
+  private async getUser(): Promise<User> {
+    const payload = this.request.user as IJwtPayload;
+    const user = await this.userRepository.findOne({
+      identity: payload.user_identity,
+    });
+    return user;
+  }
+}
