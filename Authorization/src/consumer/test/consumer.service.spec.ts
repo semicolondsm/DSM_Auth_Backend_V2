f74174eec92911d06d9fc6b7e1144bc3e3045479
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-11 22:29:57 +0900

    UPDATE; consumer.service.ts add testing module provider

--- Authorization/src/consumer/test/consumer.service.spec.ts
+++ Authorization/src/consumer/test/consumer.service.spec.ts
@@ -1,15 +1,50 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { User } from "../../shared/user/entity/user.entity";
+import { ConsumerController } from "../consumer.controller";
 import { ConsumerService } from "../consumer.service";
+import { Consumers } from "../entity/consumer.entity";
+import { RegistrationDto } from "../dto/registration.dto";
+
+class MockConsumerRepository {
+  public async registration(
+    dto: RegistrationDto,
+    user: User,
+  ) {
+
+  }
+}
+
+class MockUserRepository {
+  public async checkExist(identity: string) {
+
+  }
+
+  public async findOne() {
+
+  }
+}
 
 describe("ConsumerService", () => {
   let service: ConsumerService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ConsumerService],
+      providers: [
+        ConsumerService,
+        {
+          provide: getRepositoryToken(Consumers),
+          useClass: MockConsumerRepository,
+        },
+        {
+          provide: getRepositoryToken(User),
+          useClass: MockUserRepository,
+        },
+      ],
+      controllers: [ConsumerController],
     }).compile();
 
-    service = module.get<ConsumerService>(ConsumerService);
+    service = await module.resolve<ConsumerService>(ConsumerService);
   });
 
   it("should be defined", () => {
