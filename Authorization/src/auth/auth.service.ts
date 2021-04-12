43fd3c3b4eb3d31733685bc7182abb3d3a862b46
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-12 23:09:21 +0900

    UPDATE; auth.service.ts add signup service

--- Authorization/src/auth/auth.service.ts
+++ Authorization/src/auth/auth.service.ts
@@ -1,21 +1,22 @@
 import { Injectable, Logger } from "@nestjs/common";
-import { asyncFuncRedisSet } from "../redis.client";
+import { asyncFuncRedisGet, asyncFuncRedisSet } from "../redis.client";
 import { User } from "../shared/user/entity/user.entity";
 import {
   alreadySignupException,
+  badRequestException,
   notAllowedIDException,
   notFoundEmailException,
+  unauthorizedCodeException,
 } from "../shared/exception/exception.index";
 import { sendMail } from "../shared/mail/mail.transport";
-import { UserService } from "../shared/user/user.service";
 import { UserRepository } from "../shared/user/entity/user.repository";
+import { SignUpDto } from "./dto/sign-up.dto";
 import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly userRepository: UserRepository,
-    private readonly userService: UserService,
   ) {}
 
   public async checkAllowedId(identity: string) {
@@ -37,6 +38,23 @@ export class AuthService {
     const authNum: string = Math.floor(Math.random() * 1000000).toString();
     this.setAuthNumberForEamil(email, authNum);
     this.sendEmailWithAuthNumber(email, authNum);
+  }
+
+  public async userSignUp({ name, email, authcode, id, password }: SignUpDto) {
+    const storedData: string = await asyncFuncRedisGet(email);;
+    if (!storedData || storedData !== authcode) {
+      throw unauthorizedCodeException;
+    }
+    const getUserPromise: Promise<User> = this.userRepository.findByNameAndEmail(name, email);
+    const hashPasswordPromise: Promise<string> = bcrypt.hash(password, 12);
+    const exUser = await getUserPromise;
+    if(!exUser) {
+      throw notFoundEmailException;
+    }
+    if(exUser.password) {
+      throw alreadySignupException;
+    }
+    await this.userRepository.save({ ...exUser, identity: id, password: await hashPasswordPromise });
   }
 
   public async setAuthNumberForEamil(email: string, authNum: string) {
