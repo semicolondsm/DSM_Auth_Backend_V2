77f601685839095e595bda81fd27d34dff011ffb
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-12 16:54:10 +0900

    UPDATE; check-email.dto.ts, check-id.dto.ts add length validation

--- Authorization/src/auth/dto/check-email.dto.ts
+++ Authorization/src/auth/dto/check-email.dto.ts
@@ -1,6 +1,7 @@
-import { IsString } from "class-validator";
+import { IsString, Length } from "class-validator";
 
 export class CheckEmailDto {
   @IsString()
+  @Length(5, 50)
   email: string;
 }
--- Authorization/src/auth/dto/check-id.dto.ts
+++ Authorization/src/auth/dto/check-id.dto.ts
@@ -1,6 +1,7 @@
-import { IsString } from "class-validator";
+import { IsString, Length } from "class-validator";
 
 export class CheckIdDto {
   @IsString()
+  @Length(1, 40)
   id: string;
 }
