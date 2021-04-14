5036546e9f2224347d2acdce8b517cafb1e75f24
sonchaegeon <thscorjs1002@naver.com>
2021-04-14 22:02:19 +0900

    UPDATE; app myservice service

--- Authorization/src/app/app.service.ts
+++ Authorization/src/app/app.service.ts
@@ -1,8 +1,29 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable, Scope } from "@nestjs/common";
+import { REQUEST } from "@nestjs/core";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Consumer } from "../consumer/entity/consumer.entity";
+import { ConsumerRepository } from "../consumer/entity/consumer.repository";
+import { notFoundUserException } from "../shared/exception/exception.index";
+import { User } from "../shared/user/entity/user.entity";
+import { UserRepository } from "../shared/user/entity/user.repository";
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class AppService {
-  getHello(): string {
-    return "Hello World!";
+  constructor(
+    @InjectRepository(Consumer)
+    private readonly consumerRepository: ConsumerRepository,
+    @InjectRepository(User)
+    private readonly userRepository: UserRepository,
+    @Inject(REQUEST) private request,
+  ) {}
+
+  public async myService(): Promise<Consumer[]> {
+    const user: User = await this.userRepository.findByRequest(
+      this.request.user,
+    );
+    if (!user) {
+      throw notFoundUserException;
+    }
+    return await this.consumerRepository.myService(user.id);
   }
 }
