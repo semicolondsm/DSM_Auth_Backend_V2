8eaea7d3bcd13c97535cf0418c9d585c9bb80cab
sonchaegeon <thscorjs1002@naver.com>
2021-04-14 22:05:39 +0900

    CREATE; consumer dummy

---
+++ Authorization/src/shared/mock/dummy/consumer.dummy.ts
@@ -0,0 +1,27 @@
+export const consumers = [
+  {
+    name: "ddyzd",
+    domain_url: "ddyzd.com",
+    redirect_url: "admin.ddyzd.com",
+    client_id: "testuuid",
+    client_secret: "testuuid",
+  },
+  {
+    name: "auth",
+    domain_url: "dsmauth.com",
+    redirect_url: "test.dsmauth.com",
+    client_id: "testuuid",
+    client_secret: "testuuid",
+  },
+];
+
+export const consumersList = [
+  {
+    name: "naver",
+    domain_url: "naver.com",
+  },
+  {
+    name: "google",
+    domain_url: "google.com",
+  },
+];
