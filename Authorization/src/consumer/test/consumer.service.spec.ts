5add445353a56a59f8722a2ebc23a3671655d4c7
sonchaegeon <thscorjs1002@naver.com>
2021-04-14 22:03:35 +0900

    REPACTOR; consumer controller test
    Mock class moved

--- Authorization/src/consumer/test/consumer.service.spec.ts
+++ Authorization/src/consumer/test/consumer.service.spec.ts
@@ -1,56 +1,12 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { getRepositoryToken } from "@nestjs/typeorm";
 import { User } from "../../shared/user/entity/user.entity";
-import { ConsumerController } from "../consumer.controller";
 import { ConsumerService } from "../consumer.service";
 import { Consumer } from "../entity/consumer.entity";
-import { RegistrationDto } from "../dto/registration.dto";
 import { REQUEST } from "@nestjs/core";
-
-class MockConsumerRepository {
-  public registration(dto: RegistrationDto, user: User) {
-    const client_id: string = "testuuid";
-    const client_secret: string = "testuuid";
-
-    console.log(`dto: ${JSON.stringify(dto)}`);
-    console.log(`user: ${JSON.stringify(user)}`);
-    return { client_id, client_secret };
-  }
-
-  public list() {
-    const consumers = [
-      {
-        name: "naver",
-        domain_url: "naver.com",
-      },
-      {
-        name: "google",
-        domain_url: "google.com",
-      },
-    ];
-
-    return consumers;
-  }
-}
-
-class MockUserRepository {
-  public async findOne({ identity }) {
-    let user = new User();
-    user.identity = "testIdentity";
-
-    if (identity === "testIdentity") {
-      return user;
-    } else {
-      console.log(identity);
-    }
-  }
-}
-
-class MockRequest {
-  public user = {
-    user_identity: "testIdentity",
-  };
-}
+import { MockConsumerRepository } from "../../shared/mock/consumer.mock";
+import { MockUserRepository } from "../../shared/mock/user.mock";
+import { MockRequest } from "../../shared/mock/request.mock";
 
 describe("ConsumerService", () => {
   let service: ConsumerService;
@@ -72,7 +28,6 @@ describe("ConsumerService", () => {
           useClass: MockRequest,
         },
       ],
-      controllers: [ConsumerController],
     }).compile();
 
     service = await module.resolve<ConsumerService>(ConsumerService);
