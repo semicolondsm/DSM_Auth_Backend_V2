c0424c17d2e57b146c8dfea0e0fd022ffc7294b9
sonchaegeon <thscorjs1002@naver.com>
2021-04-14 22:01:12 +0900

    UPDATE; app myservice controller test

--- Authorization/src/app/test/app.controller.spec.ts
+++ Authorization/src/app/test/app.controller.spec.ts
@@ -1,22 +1,38 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { MockAppService } from "../../shared/mock/app.mock";
 import { AppController } from "../app.controller";
 import { AppService } from "../app.service";
 
 describe("AppController", () => {
-  let appController: AppController;
+  let controller: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        {
+          provide: AppService,
+          useClass: MockAppService,
+        },
+      ],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    controller = await app.resolve<AppController>(AppController);
+  });
+  it("should be defined", () => {
+    expect(controller).toBeDefined();
   });
 
-  describe("root", () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe("Hello World!");
+  describe("myService", () => {
+    it("should be return consumer array", () => {
+      controller.myService().then((res) => {
+        expect(res).toBeInstanceOf(Array);
+        expect(res[0].name).toEqual("ddyzd");
+        expect(res[0].domain_url).toEqual("ddyzd.com");
+
+        expect(res[1].name).toEqual("auth");
+        expect(res[1].domain_url).toEqual("dsmauth.com");
+      });
     });
   });
 });
