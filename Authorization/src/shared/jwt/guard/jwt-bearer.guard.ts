eee7c048572f9df9682b6999db780d8da07a191d
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-15 11:16:28 +0900

    CREATE; jwt-bearer.guard.ts bearer token verify

---
+++ Authorization/src/shared/jwt/guard/jwt-bearer.guard.ts
@@ -0,0 +1,15 @@
+import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common";
+
+@Injectable()
+export class JwtBearerGuard implements CanActivate {
+  constructor(private header_name: string) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    if(!request.headers[this.header_name]) {
+      return false;
+    }
+    request.headers[this.header_name] = request.headers[this.header_name].slice(7);
+    return true;
+  }
+}
