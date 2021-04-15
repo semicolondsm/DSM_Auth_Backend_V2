015edb0638a03d31af8a7478de083bd2fd4b7c37
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-15 23:28:14 +0900

    UPDATE; user.mock.ts mock user repository

--- Authorization/src/shared/mock/user.mock.ts
+++ Authorization/src/shared/mock/user.mock.ts
@@ -1,3 +1,4 @@
+import { SignUpDto } from "../../auth/dto/sign-up.dto";
 import { IJwtPayload } from "../jwt/interface/jwt-payload.interface";
 import { User } from "../user/entity/user.entity";
 
@@ -14,7 +15,49 @@ export class MockUserRepository {
     }
   }
 
-  public findOne({ where: { identity } }) {}
+  public async findOne({ where: { identity } });
+  public async findOne({ where: { email } }): Promise<User>;
+
+  public async findOne(args: any): Promise<User> {
+    if (args.where) {
+      if (args.where.email) {
+        const email: string = args.where.email;
+        if (email === "201216jjw@dsm.hs.kr") {
+          return new User();
+        } else if (email === "alreadysignupeamil@dsm.hs.kr") {
+          const user = new User();
+          user.password = "exist";
+          return user;
+        }
+      } else if (args.where.identity) {
+        const identity: string = args.where.identity;
+      }
+    }
+  }
+
+  public async checkExist(identity: string): Promise<boolean> {
+    if (identity === "tester") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public async findByNameAndEmail(name: string, email: string): Promise<User> {
+    if (email === "rightKey") {
+      const user = new User();
+      if (name === "tester") {
+        return user;
+      } else if (name === "signedTester") {
+        user.password = "alreadySignupPassword";
+        return user;
+      }
+    }
+  }
+
+  public async save(user: SignUpDto) {
+    expect(user).toEqual({ identity: "id", password: "hashedpassword" });
+  }
 }
 
 export class MockUserService {
