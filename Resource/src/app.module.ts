bae7f5064e5a3be7915b0a72b3959fc820d048ef
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-22 17:10:39 +0900

    CREATE; environment-config.module.ts

--- Resource/src/app.module.ts
+++ Resource/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { EnvironmentConfigService } from "./infrastructure/config/environment-config/environment-config.service";
+import { EnvironmentConfigModule } from "./infrastructure/config/environment-config/environment-config.module";
 
 @Module({
-  imports: [],
+  imports: [EnvironmentConfigModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, EnvironmentConfigService],
 })
 export class AppModule {}
