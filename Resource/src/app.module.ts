1a509cc69c7d85cf76834762a02340a8e4aa32ae
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-24 15:42:33 +0900

    CREATE; typeorm.module.ts

--- Resource/src/app.module.ts
+++ Resource/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { EnvironmentConfigService } from "./infrastructure/config/environment-config/environment-config.service";
 import { EnvironmentConfigModule } from "./infrastructure/config/environment-config/environment-config.module";
+import { TypeormModule } from "./infrastructure/config/typeorm/typeorm.module";
 
 @Module({
-  imports: [EnvironmentConfigModule],
-  controllers: [AppController],
-  providers: [AppService, EnvironmentConfigService],
+  imports: [EnvironmentConfigModule, TypeormModule],
 })
 export class AppModule {}
