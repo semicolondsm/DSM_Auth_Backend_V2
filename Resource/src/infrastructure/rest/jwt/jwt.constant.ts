7f23fd384f18d1353f0b91026184f00add21f156
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-26 08:44:09 +0900

    CREATE; jwt-passport infrastructure

---
+++ Resource/src/infrastructure/rest/jwt/jwt.constant.ts
@@ -0,0 +1,5 @@
+import "dotenv/config";
+
+export const ACCESS_TOKEN_EXPIRED_TIME = process.env.ACCESS_TOKEN_EXPIRED_TIME;
+export const JWT_SECRET_KEY = process.env.JWT_SECRET;
+export const ACCESS_TOKEN_HEADER = "access-token";
