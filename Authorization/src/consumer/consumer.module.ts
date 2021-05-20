040a2829c1583037dd8001a7bb10e50a0bd4bfbe
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-05-20 19:29:49 +0900

    UPDATE; consumer.module.ts UserRepository Dependency 제거

--- Authorization/src/consumer/consumer.module.ts
+++ Authorization/src/consumer/consumer.module.ts
@@ -3,15 +3,16 @@ import { ConsumerService } from "./consumer.service";
 import { ConsumerController } from "./consumer.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConsumerRepository } from "./entity/consumer.repository";
-import { UserRepository } from "../shared/user/entity/user.repository";
 import { JwtStrategy } from "../shared/jwt/passport/jwt.strategy";
 import { RedirectModule } from "../redirect/redirect.module";
+import { UserModule } from "../shared/user/user.module";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ConsumerRepository, UserRepository]),
+    TypeOrmModule.forFeature([ConsumerRepository]),
     JwtStrategy,
     RedirectModule,
+    UserModule
   ],
   providers: [ConsumerService],
   controllers: [ConsumerController],
