f516fba09819b6dd018ce49350bdf65ceb58eff3
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-05-22 22:04:05 +0900

    REFACTOR; redirect.service.ts

--- Authorization/src/redirect/entity/redirect.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { Redirect } from "./redirect.entity";
-
-@EntityRepository(Redirect)
-export class RedirectRepository extends Repository<Redirect> {}
