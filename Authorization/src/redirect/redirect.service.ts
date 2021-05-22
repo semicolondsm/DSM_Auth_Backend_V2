44e338c9ae03574fdcbeac522738d9a633ad986b
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-05-22 22:04:05 +0900

    REFACTOR; redirect.service.ts

--- Authorization/src/redirect/redirect.service.ts
+++ Authorization/src/redirect/redirect.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from "@nestjs/common";
+import { EntityManager, TransactionManager } from "typeorm";
 import { Consumer } from "../consumer/entity/consumer.entity";
-import { RedirectRepository } from "./entity/redirect.repository";
+import { Redirect } from "./entity/redirect.entity";
 
 @Injectable()
 export class RedirectService {
-  constructor(private readonly redirectRepository: RedirectRepository) {}
+  constructor() {}
 
-  public createNewRedirectUrl(redirect_url: string, consumer: Consumer) {
-    return this.redirectRepository.save({
-      consumer,
-      redirect_url,
-    });
+  public createNewRedirectUrl(
+    @TransactionManager() manager: EntityManager,
+    redirect_url: string,
+    consumer: Consumer,
+  ) {
+    return manager.save(Redirect, { redirect_url, consumer });
   }
 }
