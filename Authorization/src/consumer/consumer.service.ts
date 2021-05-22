5587cc36585055d60f0199c9b12c13ea112ce81f
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-05-22 22:03:23 +0900

    REFACTOR; consumer.service.ts

--- Authorization/src/consumer/consumer.service.ts
+++ Authorization/src/consumer/consumer.service.ts
@@ -13,6 +13,8 @@ import { Consumer } from "./entity/consumer.entity";
 import { ConsumerRepository } from "./entity/consumer.repository";
 import { RedirectService } from "../redirect/redirect.service";
 import { UserService } from "../shared/user/user.service";
+import { v4 } from "uuid";
+import { Connection, EntityManager } from "typeorm";
 
 @Injectable()
 export class ConsumerService {
@@ -20,38 +22,70 @@ export class ConsumerService {
     private readonly consumerRepository: ConsumerRepository,
     private readonly userServie: UserService,
     private readonly redirectService: RedirectService,
+    private readonly connection: Connection,
   ) {}
 
-  public async registration(
-    dto: RegistrationDto,
-    user_identity: string
+  public async registrateConsumer(
+    { consumer: consumer_name, domain_url, redirect_url }: RegistrationDto,
+    user_identity: string,
   ): Promise<RegistrationResponseData> {
     const user: User = await this.userServie.findByIdentity(user_identity);
     if (!user) {
       throw notFoundUserException;
     }
-    const consumerRecord = await this.consumerRepository.registration(
-      dto,
-      user,
-    );
-    await this.addConsumerRedirectUrl({
-      client_id: consumerRecord.client_id,
-      redirect_url: dto.redirect_url,
-    });
-    return consumerRecord;
+    const client_id: string = v4().replace(/-/g, "");
+    const client_secret: string = v4().replace(/-/g, "");
+    const queryRunner = this.connection.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      const consumer: Consumer = await this.createNewConsumer(
+        queryRunner.manager,
+        {
+          user,
+          client_id,
+          client_secret,
+          name: consumer_name,
+          domain_url: domain_url,
+        },
+      );
+      await this.redirectService.createNewRedirectUrl(
+        queryRunner.manager,
+        redirect_url,
+        consumer,
+      );
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw err;
+    } finally {
+      await queryRunner.release();
+    }
+    return { client_id, client_secret };
   }
 
-  public async list(): Promise<Consumer[]> {
-    return this.consumerRepository.list();
+  public async getConsumerCatalog(): Promise<Consumer[]> {
+    return this.consumerRepository.findNameAndDomainUrl();
   }
 
-  public async addConsumerRedirectUrl(dto: urlDto) {
+  public async addConsumerRedirectUrl({ client_id, redirect_url }: urlDto) {
     const consumer = await this.consumerRepository.findOne({
-      client_id: dto.client_id,
+      client_id: client_id,
     });
     if (!consumer) {
       throw notFoundConsumerException;
     }
-    await this.redirectService.createNewRedirectUrl(dto.redirect_url, consumer);
+    await this.redirectService.createNewRedirectUrl(
+      this.connection.manager,
+      redirect_url,
+      consumer,
+    );
+  }
+
+  private createNewConsumer(
+    manager: EntityManager,
+    consumer: Partial<Consumer>,
+  ) {
+    return manager.save(Consumer, consumer);
   }
 }
